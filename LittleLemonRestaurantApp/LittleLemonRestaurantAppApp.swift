21ff72f625691740083cd0f5ee087a732d6b20f4
Mohamed Ali <64269343+Mohamed-Alyy@users.noreply.github.com>
2023-02-19 22:04:38 +0300

    Initial Commit

---
+++ LittleLemonRestaurantApp/LittleLemonRestaurantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonRestaurantAppApp.swift
+//  LittleLemonRestaurantApp
+//
+//  Created by Mohamed Ali on 19/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonRestaurantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
